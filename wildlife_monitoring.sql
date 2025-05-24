0b4c3277cd71030227ed494fd56e033518b0977b
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-25 00:48:28 +0600

    Create rangers table

--- wildlife_monitoring.sql
+++ wildlife_monitoring.sql
@@ -1,3 +1,11 @@
 -- Active: 1747409020969@@127.0.0.1@5432@conservation_db
 -- Create Database  Wildlife Conservation Monitoring
 CREATE DATABASE conservation_db;
+
+-- Create rangers table
+CREATE TABLE rangers (
+ranger_id SERIAL PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+location TEXT,
+phone VARCHAR(20)
+)
