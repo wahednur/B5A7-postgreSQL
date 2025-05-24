5194d1408e69d7686e2a9f718960126973a84f9c
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-25 00:58:56 +0600

    Create Species table

--- wildlife_monitoring.sql
+++ wildlife_monitoring.sql
@@ -6,6 +6,18 @@ CREATE DATABASE conservation_db;
 CREATE TABLE rangers (
 ranger_id SERIAL PRIMARY KEY,
 name VARCHAR(100) NOT NULL,
-location TEXT,
-phone VARCHAR(20)
-)
+region VARCHAR(100) NOT NULL,
+phone VARCHAR(20) NOT NULL,
+);
+
+
+
+-- Create Species table
+CREATE TABLE species (
+species_id SERIAL PRIMARY KEY,
+common_name VARCHAR(100),
+scientific_name VARCHAR(150),
+discovery_date DATE,
+conservation_status VARCHAR(100)
+
+);
